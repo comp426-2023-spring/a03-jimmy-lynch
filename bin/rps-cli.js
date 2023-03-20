a418ee6101f82df2becca994ecb2adc130d68a60
jdlynch13 <jimmyl@brimmatech.com>
2023-03-20 12:35:06 -0400

    fixed small error

--- bin/rps-cli.js
+++ bin/rps-cli.js
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 import minimist from "minimist";
-import rps from "../lib.rpsls.js";
-import rpsls from "../lib.rpsls.js";
+import rps from "../lib/rpsls.js";
+import rpsls from "../lib/rpsls.js";
 
 let args = minimist(process. argv.slice(2));
 
